875e6a66db7f471cb235256eb1d61fe08dddc38e
zolotarevskyi <ofigenchik@gmail.com>
2026-01-29 11:03:59 +0200

    Update variables.tf

--- terraform/admin-vm/variables.tf
+++ terraform/admin-vm/variables.tf
@@ -8,6 +8,11 @@ variable "kops_state_bucket" {
   description = "S3 bucket where Kops stores the cluster state"
 }
 
+variable "kops_cluster_name" {
+  type        = string
+  description = "kOps cluster name, e.g. k8s.asap.im"
+}
+
 variable "kops_subdomain" {
   type        = string
   description = "Hosted zone for the subdomain, e.g., k8s.example.com"
